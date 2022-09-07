78213aeb525d56dce24ba5353acfb1d076db8332
Webudvikler Hovedforløb <miv@netkravler.com>
2022-09-07 14:05:23 +0200

    final

--- assets/js/mapping.js
+++ assets/js/mapping.js
@@ -1,20 +1,3 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  //** wait until dom is fully loaded to run code inside */
-
-  let cards = document.querySelectorAll(".card");
-  /**get all elements by classname card */
-
-  cards.forEach((e) => {
-    e.addEventListener("click", () => {
-      /**for each of the found elements, listen for an onclick */
-      let id = e.getAttribute("id");
-      //** get the id attribute for the found card */
-      filterPerson(id);
-      //** filter out all persons not equal to the id */
-    });
-  });
-});
-
 const data = [
   { id: 1, name: "John Doe", job: "Unknown", gender: "male" },
   { id: 2, name: "Maria Carey", job: "Singer", gender: "female" },
@@ -23,7 +6,6 @@ const data = [
   { id: 5, name: "Margaret Thatcher", job: "Politician", gender: "female" },
   { id: 6, name: "Marie Curie", job: "kernefysiker", gender: "male" },
 ];
-
 //** array of objects containing people */
 
 const male = "https://www.w3schools.com/howto/img_avatar.png";
@@ -33,46 +15,17 @@ const female = "https://www.w3schools.com/howto/img_avatar2.png";
 const filterPerson = (id) => {
   //document.getElementById("mother").innerHTML = "";
   //console.log(data.filter((x) => x.id === id)[0]);
-  createElements(data.filter((x) => x.id == id)[0], "father");
+  createElement(data.filter((x) => x.id == id)[0], "father");
 };
 
-const createElements = (data, father) => {
+
+
+
+const createElement = (data, father) => {
+
   document.getElementById("father").innerHTML = "";
-  //** clear father on every run of this function to fill in new data */
-  let fig = document.createElement("figure");
-  fig.setAttribute("class", "card");
-  fig.setAttribute("id", data.id);
-  //fig.setAttribute("onclick", "filterPerson(" + data.id + ")");
-  let img = document.createElement("img");
-
-  img.setAttribute("src", data.gender === "male" ? male : female);
-  img.setAttribute("alt", data.name + " er ansat som " + data.job);
-
-  let art = document.createElement("article");
-  art.setAttribute("class", "container");
-
-  let h4 = document.createElement("h4");
-
-  let b = document.createElement("b");
-  let bTextNode = document.createTextNode(data.name);
-  b.appendChild(bTextNode);
-
-  let p = document.createElement("p");
-  let pTextNode = document.createTextNode(data.job);
-  p.appendChild(pTextNode);
-
-  h4.appendChild(b);
-  art.appendChild(h4);
-  art.appendChild(p);
-
-  fig.appendChild(img);
-  fig.appendChild(art);
-
-  document.getElementById(father ? "father" : "mother").appendChild(fig);
-};
-
-const createElement = (data) => {
-  document.getElementById("mother").innerHTML += `
+  
+  document.getElementById(father ? "father" : "mother").innerHTML += `
   <figure class="card" id=${data.id}>
   <img src=${data.gender === "male" ? male : female} alt=${data.name + " er ansat som " + data.job} >
   <article class="container">
@@ -89,3 +42,17 @@ data.map((person) => createElement(person));
 //** init the "app" with the first person in data array */
 
 filterPerson(data[0].id);
+
+let cards = document.querySelectorAll(".card");
+
+/*get all elements by classname card */
+
+cards.forEach((e) => {
+  e.addEventListener("click", () => {
+    /**for each of the found elements, listen for an onclick */
+    let id = e.getAttribute("id");
+    //** get the id attribute for the found card */
+    filterPerson(id);
+    //** filter out all persons not equal to the id */
+  });
+});
